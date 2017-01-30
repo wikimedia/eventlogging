abecc96cefd1ea8fdc3fa92a3bd056105069d629
Nuria <nuria@wikimedia.org>
2017-01-30 21:14:23 +0000

    Revert "Changes UA string to JSON map"
    This reverts commit 4b28b1455043585b6a0462cd3ad39c5bd8feb0e1.
    
    Change-Id: I955c36ade34e93f9ced07a21fed37e8c0a5ece26

--- eventlogging/parse.py
+++ eventlogging/parse.py
@@ -42,7 +42,6 @@ import uuid
 
 from .compat import json, unquote_plus, uuid5
 from .event import Event
-from .utils import parse_ua
 
 __all__ = (
     'LogParser', 'ncsa_to_unix',
@@ -156,9 +155,6 @@ class LogParser(object):
         event = {k: f(match.group(k)) for f, k in caster_key_pairs}
         event.update(event.pop('capsule'))
         event['uuid'] = capsule_uuid(event)
-        if ('userAgent' in event) and event['userAgent']:
-            parsed_ua = parse_ua(event['userAgent'])
-            event['userAgent'] = parsed_ua
         return Event(event)
 
     def __repr__(self):
