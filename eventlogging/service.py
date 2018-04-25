f0783bbf5c2aebe7beaacda58d30954f07d16e47
Andrew Otto <acotto@gmail.com>
2018-04-25 15:10:21 -0400

    Fix missing comma
    Change-Id: I81d9868439ba3707e4f224867352c75170f549cb

--- eventlogging/service.py
+++ eventlogging/service.py
@@ -419,7 +419,7 @@ class EventHandler(
 
     def on_connection_close(self):
         logging.warn(
-            'Client closed connection before sending events finished. Body:\n%s'
+            'Client closed connection before sending events finished. Body:\n%s',
             self.request.body
         )
 
